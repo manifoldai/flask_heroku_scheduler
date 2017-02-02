733f733a0eee07394319d6e52abb90e2f94f3052
Sourav Dey <souravdey@gmail.com>
2017-02-02 14:12:55 -0800

    Initial commit with management script and simple Flask app

---
+++ manage.py
@@ -0,0 +1,13 @@
+from flask_script import Manager
+
+from myapp import app
+
+manager = Manager(app)
+
+
+@manager.command
+def hello():
+    print("hello")
+
+if __name__ == "__main__":
+    manager.run()
